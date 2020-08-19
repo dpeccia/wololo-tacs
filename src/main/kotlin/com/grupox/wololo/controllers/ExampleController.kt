fb5092dd7eadb5dd99282ce1bdadf801823e7858
CimimUxMaio <cimimuxmaio@gmail.com>
2020-08-19 18:11:07 -0300

    Ahora el ejemplo usa templates con mustache (un poco mas interesante)

--- src/main/kotlin/com/grupox/wololo/controllers/ExampleController.kt
+++ src/main/kotlin/com/grupox/wololo/controllers/ExampleController.kt
@@ -1,14 +1,18 @@
 package com.grupox.wololo.controllers
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("")
-@RestController
+@Controller // El string retornado es el nombre del template a utilizar. Si uso @RestController retorna el literal.
 class ExampleController {
     @GetMapping
-    fun home(): String {
-        return "Hello spring boot!!!"
+    fun home(model: Model): String {
+        model["title"] = "Home page"
+        model["message"] = "Hello spring boot!"
+        return "sample"
     }
 }
