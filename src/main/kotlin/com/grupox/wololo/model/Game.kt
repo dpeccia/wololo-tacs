cede90ba3365463d3851707bb54ac2d1cffa2070
CimimUxMaio <cimimuxmaio@gmail.com>
2020-08-30 04:41:14 -0300

    Fix a mi commit anterior :S

--- src/main/kotlin/com/grupox/wololo/model/Game.kt
+++ src/main/kotlin/com/grupox/wololo/model/Game.kt
@@ -2,18 +2,7 @@ package com.grupox.wololo.model
 
 import java.util.*
 
-class Game(id: Int, status: Status) {
-    private val id: Int
-        get
-
-    private var status: Status
-        get
-
-    init {
-        this.id = id
-        this.status = status
-    }
-
-    // fun getId(): Int = id
-    // fun getStatus(): Status = status
+class Game(private val id: Int, private var status: Status) {
+    fun getId(): Int = id
+    fun getStatus(): Status = status
 }
