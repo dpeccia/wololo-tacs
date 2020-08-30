ff444a78db7421d505b1798616c642715f924ac5
CimimUxMaio <cimimuxmaio@gmail.com>
2020-08-30 04:25:22 -0300

    Fix visibilidad de los campos de Game para GameController

--- src/main/kotlin/com/grupox/wololo/model/Game.kt
+++ src/main/kotlin/com/grupox/wololo/model/Game.kt
@@ -2,7 +2,18 @@ package com.grupox.wololo.model
 
 import java.util.*
 
-class Game(private val id: Int, private val status: Status) {
-    fun getId(): Int = id
-    fun getStatus(): Status = status
+class Game(id: Int, status: Status) {
+    private val id: Int
+        get
+
+    private var status: Status
+        get
+
+    init {
+        this.id = id
+        this.status = status
+    }
+
+    // fun getId(): Int = id
+    // fun getStatus(): Status = status
 }
