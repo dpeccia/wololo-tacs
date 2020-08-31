718ab8e6de2d6b1c481220fa39ca5b726edcdff9
CimimUxMaio <cimimuxmaio@gmail.com>
2020-08-31 18:25:11 -0300

    Hago privados los setters de nombre y mail de User

--- src/main/kotlin/com/grupox/wololo/model/Game.kt
+++ src/main/kotlin/com/grupox/wololo/model/Game.kt
@@ -3,5 +3,6 @@ package com.grupox.wololo.model
 class Game(val id: Int, val province: Province, status: Status = Status.NEW) {
     var status: Status = status
         private set
+
     fun getTownById(idTown: Int): Town ? = province.getTownById(idTown)
 }
--- src/main/kotlin/com/grupox/wololo/model/User.kt
+++ src/main/kotlin/com/grupox/wololo/model/User.kt
@@ -1,6 +1,12 @@
 package com.grupox.wololo.model
 
-data class User(val id: Int, var nombre: String, var mail: String, private var password: String, val esAdmin: Boolean) {
+class User(val id: Int, nombre: String, mail: String, private var password: String, val esAdmin: Boolean) {
+    var nombre: String = nombre
+        private set
+
+    var mail: String = mail
+        private set
+
     // TODO: MAKE THIS COMPARATION WITH ENCRYPTED PASSWORDS
     fun isUserByLoginData(loginData: LoginModel): Boolean = this.mail == loginData.mail && this.password == loginData.password
 }
