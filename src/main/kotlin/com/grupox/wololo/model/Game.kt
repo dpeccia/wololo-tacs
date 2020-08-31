46191942fb89a35030c97fd10dae90c7fa9cca47
PabloOve <pablooib81@gmail.com>
2020-08-31 01:33:34 -0300

    cambio chiquito
    var Province (en vez de val) en la clase Game

--- src/main/kotlin/com/grupox/wololo/model/Game.kt
+++ src/main/kotlin/com/grupox/wololo/model/Game.kt
@@ -4,7 +4,7 @@ import com.grupox.wololo.model.dtos.DTO
 import com.grupox.wololo.model.dtos.Entity
 import com.grupox.wololo.model.dtos.GameDTO
 
-class Game(val id: Int, private var status: Status, val province: Province) : Entity {
+class Game(val id: Int, private var status: Status, var province: Province) : Entity {
     override fun getDTO(): DTO = GameDTO(id, status, province)
 
     fun getTownById(idTown: Int): Town ?= province.getTownById(idTown)
