24f5de67848d25be67959d02ee82e77269e3dbe9
CimimUxMaio <cimimuxmaio@gmail.com>
2020-09-02 20:29:50 -0300

    Mas pequeñas simplificaciones para que sea mas entendible

--- src/main/kotlin/com/grupox/wololo/model/services/GeoRef.kt
+++ src/main/kotlin/com/grupox/wololo/model/services/GeoRef.kt
@@ -6,11 +6,13 @@ import arrow.core.extensions.list.foldable.foldLeft
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.grupox.wololo.model.Coordinates
 import com.grupox.wololo.model.Province
 import com.grupox.wololo.model.Town
 import io.github.rybalkinsd.kohttp.ext.httpGet
+import io.github.rybalkinsd.kohttp.jackson.ext.toJsonOrNull
 import io.github.rybalkinsd.kohttp.jackson.ext.toType
 import okhttp3.Response
 
@@ -34,42 +36,41 @@ data class LocationData (
 )
 
 object GeoRef {
-    private const val exactValue: Boolean = true
-    private const val maxMatches: Int = 200 // Numero maximo de resultados que devuelve una query a georef
     private const val provinceDataUrl = "https://apis.datos.gob.ar/georef/api/provincias"
     private const val townsDataUrl = "https://apis.datos.gob.ar/georef/api/municipios"
+    private const val exactValue: Boolean = true // Las busquedas por nombres buscan el match exacto
+    private const val maxMatches: Int = 200 // Numero maximo de resultados que devuelve una query a georef
 
     fun generateProvince(provinceName: String): Option<Province> {
         return httpGetProvinceData(provinceName).map {
             Province(
-                    it.id,
-                    it.name,
-                    ArrayList(httpGetTownsData(it.id).map { townData -> Town(townData.id, townData.name, townData.coordinates) }),
-                    it.coordinates
+                it.id,
+                it.name,
+                ArrayList(httpGetTownsData(it.id).map { townData -> Town(townData.id, townData.name, townData.coordinates) }),
+                it.coordinates
             )
         }
     }
 
     private fun httpGetProvinceData(name: String): Option<LocationData> {
-        val queryData: Option<GeoRefResponse.ProvinceQuery> =
-                httpGetQueryData(provinceDataUrl, mapOf("nombre" to name, "exacto" to exactValue.toString()))
+        val responseData: Option<GeoRefResponse.ProvinceQuery> =
+            httpGetQueryData(provinceDataUrl, mapOf("nombre" to name, "exacto" to exactValue.toString()))
 
-        return queryData.mapNotNull { it.matches.firstOrNull() }
+        return responseData.mapNotNull { it.matches.firstOrNull() }
     }
 
-    private fun httpGetTownsData(provinceId: Int): List<LocationData> =
-        httpGetQueryData<GeoRefResponse.TownsQuery>(townsDataUrl, mapOf("provincia" to provinceId.toString(), "max" to maxMatches.toString()))
-            .toList().flatMap { it.matches }
+    private fun httpGetTownsData(provinceId: Int): List<LocationData> {
+        val responseData: Option<GeoRefResponse.TownsQuery> =
+            httpGetQueryData(townsDataUrl, mapOf("provincia" to provinceId.toString(), "max" to maxMatches.toString()))
+
+        return responseData.toList().flatMap { it.matches }
+    }
 
     private inline fun <reified QueryDataT : GeoRefResponse>httpGetQueryData(url: String, queryParams: Map<String, String>): Option<QueryDataT> {
-        val finalUrl = appendQueryParams(url, queryParams)
-        println(finalUrl)
-        val queryResponse: Response = finalUrl.httpGet()
-        val mapper = ObjectMapper().registerKotlinModule()
-        return Some(queryResponse).mapNotNull { it.toType<QueryDataT>(mapper) }
+        val queryResponse: Response = appendQueryParams(url, queryParams).httpGet()
+        return Some(queryResponse).mapNotNull { it.toType<QueryDataT>() }
     }
 
     private fun appendQueryParams(url: String, queryParams: Map<String, String>): String =
         queryParams.toList().foldLeft("$url?") { unf, (key, value) -> "$unf$key=$value&" }.removeSuffix("&")
-
 }
