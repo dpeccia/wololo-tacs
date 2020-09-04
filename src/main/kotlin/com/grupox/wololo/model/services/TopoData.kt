f2a50014d670a2b1b5f3a748f41806e6eaa4bd47
CimimUxMaio <cimimuxmaio@gmail.com>
2020-09-04 20:33:21 -0300

    Servicio TopoData

---
+++ src/main/kotlin/com/grupox/wololo/model/services/TopoData.kt
@@ -0,0 +1,26 @@
+package com.grupox.wololo.model.services
+
+import arrow.core.*
+import arrow.core.extensions.either.monad.flatMap
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.grupox.wololo.errors.CustomException
+import com.grupox.wololo.model.Coordinates
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private data class TopoDataResponse(val results: List<ElevationData>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private data class ElevationData(val elevation: Float)
+
+object TopoData : HttpService("TopoData") {
+    private const val baseUrl: String = "http://api.opentopodata.org/v1/test-dataset"
+
+    private fun requestElevation(coordinates: Coordinates): Either<CustomException, Float> {
+        val queryResponse: Either<CustomException, TopoDataResponse> =
+            requestData(baseUrl, mapOf("locations" to "${coordinates.latitude},${coordinates.longitude}"))
+
+        return queryResponse
+                .filterOrOther({ it.results.isNotEmpty() }, { CustomException.BadDataFromExternalRequestException("Theres is no data for coordinates: $coordinates in $apiName API") })
+                .map { it.results.first().elevation }
+    }
+}
