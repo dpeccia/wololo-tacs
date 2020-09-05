38d13275d647aafa843b06b9b68f3019a6855946
CimimUxMaio <cimimuxmaio@gmail.com>
2020-09-05 15:43:47 -0300

    Creo estructura para el servicio de pixabay

---
+++ src/main/kotlin/com/grupox/wololo/model/services/Pixabay.kt
@@ -0,0 +1,25 @@
+package com.grupox.wololo.model.services
+
+import arrow.core.Either
+import com.grupox.wololo.errors.CustomException
+
+
+object Pixabay : HttpService(apiName = "Pixabay"){
+    private const val baseUrl = "https://pixabay.com/api/"
+    private const val apiKey = "18188600-5dcc119bdfe90fa9bca9531b9" // esto no deberia ir aca! Es solo de prueba
+    private val configParams: Map<String, String> = mapOf(
+                                                        "key" to apiKey,
+                                                        "lang" to "es",
+                                                        "image_type" to "photo",
+                                                        "category" to "places"
+                                                    )
+
+    private fun requestImage(locationName: String): Either<CustomException, String> {
+        val queryParams = configParams.toMutableMap()  // TODO: No me gusta esto :S
+        queryParams.putIfAbsent("p", locationName)
+        val requestResponse: Either<CustomException, Float> = requestData(baseUrl, queryParams)
+        TODO()
+    }
+
+    private fun urlFormat(str: String) = str.replace(' ', '+')
+}
