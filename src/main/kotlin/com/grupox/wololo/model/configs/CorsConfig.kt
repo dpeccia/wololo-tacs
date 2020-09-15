b548e02ff8b412ffd6b502a25dd8d9b83f0c5062
PabloOve <pablooib81@gmail.com>
2020-09-14 23:10:14 -0300

    Cors config
    .

---
+++ src/main/kotlin/com/grupox/wololo/model/configs/CorsConfig.kt
@@ -0,0 +1,17 @@
+package com.grupox.wololo.model.configs
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig(@Value("\${frontend.url}") val frontendurl: String) : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+                .addMapping("*")
+                .allowedMethods("OPTIONS", "GET", "PUT", "POST", "DELETE")
+                .allowedOrigins(frontendurl)
+                .allowedHeaders("*")
+    }
+}
